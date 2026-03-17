b8e2bfa55652cc065d4f4e5845ba4510e84d5344
idreytser-kryptowire <106192519+idreytser-kryptowire@users.noreply.github.com>
2026-03-17 10:09:49 -0400

    Update index.js
    oops

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
       "app": fs.createReadStream( pathToFile ),
       "category": category
     }
-    const theHeaders: {
+    const theHeaders = {
        'Authorization':"Bearer " + apiKey
     }
 
